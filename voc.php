21757a182e2d25c5647b2e928f5eae99902f5bf4
atrizzstore <73925979+atrizzstore@users.noreply.github.com>
2020-11-17 16:40:05 +0700

    Update voc.php

--- voc.php
+++ voc.php
@@ -1,140 +1,73 @@
-<?php
-/**
- * Class to handle coupon operations
- * Changes by Alex Rabinovich (@putchi)
- * 
- * @author Joash Pereira
- * @date  2015-06-05
- */
-class coupon {
-    CONST MIN_LENGTH = 4;
-    
-    /**
-     * MASK FORMAT [XXX-XXX]
-     * 'X' this is random symbols
-     * '-' this is separator
-     *
-     * @param array $options
-     * @return string
-     * @throws Exception
-     */
-    static public function generate($options = []) {
-        
-        // to accept options as function arguments like on README
-        if (!is_array($options) && func_num_args() > 0) {
-            $keys = ['length', 'prefix', 'suffix', 'letters', 'numbers', 'symbols', 'mixed_case', 'mask'];
-            $opt = [];
-            foreach (func_get_args() as $key => $value) {
-                $opt[ $keys[ $key ] ] = $value;
-            }
-            $options = $opt;
-        }
-
-        $length         = (isset($options['length']) ? filter_var($options['length'], FILTER_VALIDATE_INT, ['options' => ['default' => self::MIN_LENGTH, 'min_range' => 1]]) : self::MIN_LENGTH );
-        $prefix         = (isset($options['prefix']) ? self::cleanString(filter_var($options['prefix'], FILTER_SANITIZE_STRING)) : '' );
-        $suffix         = (isset($options['suffix']) ? self::cleanString(filter_var($options['suffix'], FILTER_SANITIZE_STRING)) : '' );
-        $useLetters     = (isset($options['letters']) ? filter_var($options['letters'], FILTER_VALIDATE_BOOLEAN) : true );
-        $useNumbers     = (isset($options['numbers']) ? filter_var($options['numbers'], FILTER_VALIDATE_BOOLEAN) : false );
-        $useSymbols     = (isset($options['symbols']) ? filter_var($options['symbols'], FILTER_VALIDATE_BOOLEAN) : false );
-        $useMixedCase   = (isset($options['mixed_case']) ? filter_var($options['mixed_case'], FILTER_VALIDATE_BOOLEAN) : false );
-        $mask           = (isset($options['mask']) ? filter_var($options['mask'], FILTER_SANITIZE_STRING) : false );
-
-        $uppercase    = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'];
-        $lowercase    = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'];
-        $numbers      = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-        $symbols      = ['`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '\\', '|', '/', '[', ']', '{', '}', '"', "'", ';', ':', '<', '>', ',', '.', '?'];
-
-        $characters   = [];
-        $coupon = '';
-
-        if ($useLetters) {
-            if ($useMixedCase) {
-                $characters = array_merge($characters, $lowercase, $uppercase);
-            } else {
-                $characters = array_merge($characters, $uppercase);
-            }
-        }
-
-        if ($useNumbers) {
-            $characters = array_merge($characters, $numbers);
-        }
-
-        if ($useSymbols) {
-            $characters = array_merge($characters, $symbols);
-        }
-
-        if ($mask) {
-            for ($i = 0; $i < strlen($mask); $i++) {
-                if ($mask[$i] === 'X') {
-                    $coupon .= $characters[mt_rand(0, count($characters) - 1)];
-                } else {
-                    $coupon .= $mask[$i];
-                }
-            }
-        } else {
-            for ($i = 0; $i < $length; $i++) {
-                $coupon .= $characters[mt_rand(0, count($characters) - 1)];
-            }
-        }
-
-        return $prefix . $coupon . $suffix;
-    }
-
-    /**
-     * @param int $maxNumberOfCoupons
-     * @param array $options
-     * @return array
-     */
-    static public function generate_coupons($maxNumberOfCoupons = 1, $options = []) {
-        $coupons = [];
-        for ($i = 0; $i < $maxNumberOfCoupons; $i++) {
-            $temp = self::generate($options);
-            $coupons[] = $temp;
-        }
-        return $coupons;
-    }
-
-    /**
-     * @param int $maxNumberOfCoupons
-     * @param $filename
-     * @param array $options
-     */
-    static public function generate_coupons_to_xls($maxNumberOfCoupons = 1, $filename, $options = []) {
-        $filename = (empty(trim($filename)) ? 'coupons' : trim($filename));
-
-        header('Content-Type: application/vnd.ms-excel');
-
-        echo 'Coupon Codes' . "\t\n";
-        for ($i = 0; $i < $maxNumberOfCoupons; $i++) {
-            $temp = self::generate($options);
-            echo $temp . "\t\n";
-        }
-
-        header('Content-disposition: attachment; filename=' . $filename . '.xls');
-    }
-
-    /**
-     * Strip all characters but letters and numbers
-     * @param $string
-     * @param array $options
-     * @return string
-     * @throws Exception
-     */
-    static private function cleanString($string, $options = []) {
-        $toUpper = (isset($options['uppercase']) ? filter_var($options['uppercase'], FILTER_VALIDATE_BOOLEAN) : false);
-        $toLower = (isset($options['lowercase']) ? filter_var($options['lowercase'], FILTER_VALIDATE_BOOLEAN) : false);
-
-        $striped = preg_replace('/[^a-zA-Z0-9]/', '', trim($string));
-
-        // make uppercase
-        if ($toLower && $toUpper) {
-            throw new Exception('You cannot set both options (uppercase|lowercase) to "true"!');
-        } else if ($toLower) {
-            return strtolower($striped);
-        } else if ($toUpper) {
-            return strtoupper($striped);
-        } else {
-            return $striped;
-        }
-    }
+function gas($voc){
+date_default_timezone_set('Asia/Jakarta');
+$url="https://gql.tokopedia.com";
+echo "[!] Test VOC: {$voc}\n";
+$data='[
+  {
+    "variables": {
+      "data": {
+        "product_id": 747,
+        "price": 40000,
+        "code": "'.$voc.'",
+        "client_number": "no isi pulsa"
+      }
+    },
+    "operationName": null,
+    "md5": "isi md5",
+    "query": "query rechargeCheckVoucher($data: RechargeInputVoucher!) {\n    status\n    rechargeCheckVoucher(voucher: $data) {\n        data {\n          success\n          code\n          discount_amount\n          cashback_amount\n          promo_code_id\n          is_coupon\n          message {\n            state\n            color\n            text\n          }\n          title_description\n        }\n        errors{\n          status\n          title\n        }\n    }\n}\n"
+  }
+]';
+$h=array();
+$h[]='{"content-type: application/json"}';
+$h[]="x-tkpd-clc: isi value hash";
+$h[]="x-method: POST";
+$h[]="user-agent: TkpdConsumer/3.99 (Android 7.1.2;)";
+$h[]="x-user-id: isi id tokped";
+$h[]="request-method: POST";
+$h[]="authorization: isi value";
+$h[]="x-tkpd-app-version: android-3.99";
+$h[]="x-tkpd-app-name: com.tokopedia.customerappp";
+$h[]="date: Fri, ".date('d')." Nov 2020 ".date('h:i:s')." +0700";
+$h[]="os_version: 25";
+$h[]="content-md5: isi value";
+$h[]="x-release-track: production";
+$h[]="x-app-version: 320309904";
+$h[]="x-device: android-3.99";
+$h[]="tkpd-sessionid: isi value";
+$h[]="tkpd-userid: isi value";
+$h[]="fingerprint-hash: isi value";
+$h[]="accounts-authorization: isi value";
+$h[]="fingerprint-data: isi value";
+$h[]="x-ga-id: isi value";
+$h[]="content-type: application/json; charset=UTF-8";
+$h[]="content-length: ".strlen($data);
+$ch=curl_init();
+curl_setopt($ch,CURLOPT_URL,$url);
+curl_setopt($ch,CURLOPT_HTTPHEADER,$h);
+curl_setopt($ch,CURLOPT_POST,true);
+curl_setopt($ch,CURLOPT_POSTFIELDS,$data);
+curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+curl_setopt($ch,CURLOPT_ENCODING,"gzip");
+$res=json_decode(curl_exec($ch),true);
+curl_close($ch);
+$dat=$res[0]['data']['rechargeCheckVoucher']['data'];
+$status=(int) $dat['success'];
+$diskon=$dat['discount_amount'];
+$pesan=$dat['message']['text'];
+echo "[•] Voc: {$voc}\n[•] Status: {$status}\n[•] Diskon: {$diskon}\n[•] Pesan: {$pesan}\n";
+if($diskon !=0 || $status==1){
+$save=fopen('voc.txt','a');
+fwrite($save,$voc."\n");
+fclose($save);
 }
+}
+error_reporting(0);
+require_once 'voc.php';
+$i=0;
+while($i < 1000){
+$voc="TKPD30".coupon::generate(4, ””, ””, true, true);
+gas($voc);
+sleep(5);
+$i++;
+}
+   
